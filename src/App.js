0ecc8a563e56a0906f0bf961c2988f111632afb0
venkatesh <venkatesh.nowwin@gmail.com>
2020-08-14 21:26:11 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useContext, useReducer} from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import Context from "store/context";
+import reducer from "store/reducer";
+import routes from "routes"
+import Layout from "components/Layout"
+import {NotificationContainer} from 'react-notifications';
+
+import "assets/css/style.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-notifications/lib/notifications.css';
 
 function App() {
+  const initialState = useContext(Context);
+  const [state, dispatch] = useReducer(reducer, initialState);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Context.Provider value={{ state, dispatch }}>
+        <div className="bgimage">
+          <Layout>
+            <Route component={routes} />
+          </Layout>
+          <NotificationContainer/>
+        </div>
+
+      </Context.Provider>
+    </BrowserRouter>
   );
 }
 
